f827e3b74cf247528d3851b85a9a3c39a4d7a7e3
evhuang <evhuang@697-334-Bree.sf.k12.internal>
2015-10-22 10:36:09 -0700

    Finished up to cheeps and clucks

--- Chick.java
+++ Chick.java
@@ -1,4 +1,22 @@
-class Chick implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound, String sound2)     {         
+         myType = type;         
+         if((int)(Math.random() * 10) < 5){mySound = sound;}  
+        else  {mySound = sound2;}    
+     }     
+     public Chick()     {         
+         myType = "chick"; 
+         if((int)Math.random() * 10 - 5 < 0)        
+         {
+            mySound = "cheep";  
+         }  
+         else  
+         {
+            mySound = "cluck";
+         }
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,8 @@
-
-void setup()
-{
-	//your variable declarations here
+public void setup() {     
+    Cow c = new Cow("cow", "moo");   
+    Chick k = new Chick("chick", "cheep", "cluck");
+    Pig p = new Pig("pig", "oink");
+    Farm f = new Farm();
+    f.animalSounds(); 
 }
-
+    
